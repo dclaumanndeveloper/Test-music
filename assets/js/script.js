0553f53da8b46214d2d7a2e0acef239d1d5a017b
Diego Ramos Claumann <dclaumanndeveloper@protonmail.com>
2024-09-04 09:25:07 -0300

    Add Comentários no script.js para facilitação de manutenção

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,5 @@
+
+/* Criação da lista de músicas */
 const musicas = [
     { id: 1, titulo: 'Boiadeira', artista: 'Ana Castela', genero: 'Sertanejo',dataLancamento: '12/01/2021' },
     { id: 2, titulo: 'Pais do Futebol', artista: 'Skank', genero: 'Rock',dataLancamento: '12/06/2001' },
@@ -5,13 +7,14 @@ const musicas = [
     { id: 4, titulo: 'Sequestraram minha sogra', artista: 'Bezerra da Silva', genero: 'Pagode',dataLancamento: '12/01/1989' },
     { id: 5, titulo: 'Renasci das Cinzas', artista: 'MC Daniel', genero: 'Funk',dataLancamento: '21/09/2023' },
   ];
-
+/* Vinculação com os campos da tela html */
 const listaMusicas = document.getElementById('lista-musicas');
 const inputFiltro = document.getElementById('filtro');
 
+/* Renderização da tabela com os dados da música */
 function renderizarMusicas(musicasFiltradas) {
     listaMusicas.innerHTML = ''; 
-  
+    /* ForEach para criação de música por música na table */
     musicasFiltradas.forEach(musica => {
       const tr = document.createElement('tr');
 
@@ -26,7 +29,7 @@ function renderizarMusicas(musicasFiltradas) {
 
     const tdDataLancamento = document.createElement('td');
     tdDataLancamento.textContent = musica.dataLancamento;
-
+    /* Colocação das tr na table do html */
     tr.appendChild(tdTitulo);
     tr.appendChild(tdArtista);
     tr.appendChild(tdGenero);
@@ -35,7 +38,8 @@ function renderizarMusicas(musicasFiltradas) {
     listaMusicas.appendChild(tr);
     });
   }
-
+ 
+  /* Filtragem das músicas por todos os itens de uma música  */
   function filtrarMusicas(termo) {
     const musicasFiltradas = musicas.filter(musica => {
       return( musica.titulo.toLowerCase().includes(termo.toLowerCase()) || 
@@ -49,12 +53,17 @@ function renderizarMusicas(musicasFiltradas) {
     renderizarMusicas(musicasFiltradas);
   }
 
+  /* Renderização inicial da tabela ao abrir a página */
   renderizarMusicas(musicas);
-
+  
+  
+  /* Adição do evento para verificar atualização do input de filtro e filtro em tempo real enquanto digita */
   inputFiltro.addEventListener('input', () => {
     const termo = inputFiltro.value;
     filtrarMusicas(termo);
   });
+
+  /* Filtragem do Gênero ao trocar o gênero no dropdown */
   function filtrarGenero(){
    const genero = document.getElementById('genero').value;
    console.log(genero);
